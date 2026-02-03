3c2702c32cb041da83d1adbb9c29cac0e387e4da
TheSmoothWater <brejonturner@gmail.com>
2026-02-03 18:55:54 -0500

    Added filepath_functions.py

---
+++ filepath_functions.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+def returnFilePath(filePath: str) -> str:
+    inputString = repr(str)
+    print(pathname)
+    return ""
+
+pathname = r"D:\capstone_project_data"
+returnFilePath(pathname)
---
+++ filepath_notes.md
@@ -0,0 +1,4 @@
+# Pure Path
+Useless lol. Unless you're dealing with multiple os's in some way.
+
+# Concrete classes are the way to go
